babd6176a2676fd4866a6c620c941180be69ca1c
zaabta <ali.1995almanea@gmail.com>
2022-09-08 16:51:02 +0300

    qr linktype

--- routes/apiRoutesV1.js
+++ routes/apiRoutesV1.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router.use("/users", require("../lib/users/routes"))
 router.use("/subscribes", require("../lib/subscribes/routes"))
 router.use("/links", require("../lib/links/routes"))
+router.use("/linktypes", require("../lib/linkTypes/routes"))
+router.use("/qr", require("../lib/qr/routes"))
 
 module.exports = router;
